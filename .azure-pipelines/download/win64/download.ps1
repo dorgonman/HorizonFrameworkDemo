ba5bb424878e11ab90e4f1db5127c29d1355f7e5
dorgon chang <dorgonman@hotmail.com>
2019-04-04 17:19:10 +0800

    Fix VR Mode settings

---
+++ .azure-pipelines/download/win64/download.ps1
@@ -0,0 +1,15 @@
+$projectRoot=$(Resolve-Path -Path "$PSScriptRoot\..\..\..\")
+
+. "$projectRoot\ue_ci_scripts\function\ps1\az\devops.ps1"
+
+
+$cmd="DownloadArtifact " +
+    "-Organization https://hsgame.visualstudio.com/ " +
+    "-Project UE4HorizonPlugin " +
+    "-DefinitionIds 28 " +
+    "-ArtifactName drop_standalone_win64_shipping " +
+    "-DownloadToPath " + $projectRoot + "\Intermediate\PipelineArtifact\"
+
+echo $cmd
+iex $cmd
+
